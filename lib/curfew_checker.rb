1d0d76e24db2fa9229d67aa992515ade97e655da
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-25 14:05:32 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,5 +1,9 @@
 def simple_curfew_checker(time)
   if time ==11
+    puts "You're in trouble! Better get home quick!"
+  if time > 11
+    puts "You're in trouble better get home quick!"
+  end
 end
 
 simple_curfew_checker(10)
